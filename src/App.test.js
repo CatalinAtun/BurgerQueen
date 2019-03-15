5c1d6cfa525695681ba39b6311af6426c78c0cff
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-03-15 15:01:08 -0300

    Se agregan test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+import App from './App.js';
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('debería renderizar tres <div />', () => {
+    expect(shallow(<App />).find('div').length).toEqual(3);
+  });
 });
+
